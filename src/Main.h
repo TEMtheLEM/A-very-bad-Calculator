270b12a34db4a67af1f74876d221b285c80aedf0
TEMtheLEM <cinch01@outlook.com>
2022-03-27 03:12:46 -0400

    Add license notice to the top of some files

--- src/Main.h
+++ src/Main.h
@@ -1,3 +1,23 @@
+/**
+ * Main.h - Template (header) for Main.cpp
+ * Copyright (C) 2022  Connor Inch
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+
 #pragma once
 #include <wx/wx.h>
 
@@ -6,5 +26,6 @@ class Main : public wxApp {
 public:
 	virtual bool OnInit();
 };
+
 
 wxIMPLEMENT_APP(Main);
